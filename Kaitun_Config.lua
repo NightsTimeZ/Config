2ab121e7c9b69ae6d73b31ed9ca4e58ded83bca8
NightsTimeZ <84789294+NightsTimeZ@users.noreply.github.com>
2023-01-01 00:10:23 +0700

    Create Kaitun_Config.lua

---
+++ Kaitun_Config.lua
@@ -0,0 +1,73 @@
+_G.KaitunConfig = {
+    ["Auto Boost FPS"] = true,
+    ["Skip Level Farm"] = true,
+    ["Block All Hop"] = false,
+    ["Auto Collect Present"] = true,
+    
+    ["Start Kaitun"] = true,
+    -- World 1
+    ["Auto Pole"] = true, -- จะตีเเค่ถ้ามันเกิดไม่ได้ตีจนกว่าจะได้
+    ["Auto Saber"] = true,
+    ["Auto Second World"] = true,
+    -- World 2
+    ["Auto Rengoku"] = true,
+    ["Auto Quest Flower"] = true,
+    ["Auto Bartilo"] = true,
+    ["Auto Cursed Captain"] = true,
+    ["Auto Darkbeard"] = true,
+    ["Auto Factory"] = true,
+    ["Auto Third World"] = true,
+    ["Allise Fruit"] = {"Dragon-Dragon","Soul-Soul","Venom-Venom","Dough-Dough"}, -- จะไม่ใช้ผลพวกนี้ในการเปิดประตูไปโลก3
+    -- World 3
+    ["Auto Hallow Scythe"] = true,
+    ["Auto Buddy Sword"] = true,
+    ["Auto Spikey Trident"] = true,
+    ["Auto Tushita"] = true,
+    ["Auto Elite Hunter"] = true,
+    ["Auto rip Indra"] = true,
+    ["Auto Yama"] = true,
+    ["Auto Canvander"] = true,
+    ["Auto Soul Guitar"] = true,
+    ["Auto Rainbow Haki"] = true,
+    ["Auto Cursed Dual Katana"] = true,
+    
+    -- Fighting Style 
+    
+    ["Auto God Human"] = true,
+    ["Auto Superhuman"] = true,
+    ["Auto Death Step"] = true,
+    ["Auto Sharkman Karate"] = true,
+    ["Auto Electric Claw"] = true,
+    ["Auto Dargon Talon"] = true,
+    
+    ["Auto Mastery Devil Fruit"] = true,
+    ["Skill Setting"] = {
+        ["Z"] = 0.1,
+        ["X"] = 0.1,
+        ["C"] = 0.1,
+        --["V"] = 0.1, -- อันไหนไม่เอาลบออกไปเลย
+    },
+    ["Auto Mastery Sword"] = true,
+    ["Sword Rarity Select"] = {"Mythical","Legendary"}, -- Common , Uncommon,Rare,Legendary,Mythical
+
+    ["Select Redeem Code Level"] = 1,
+    ["Select Rate Fruit Rais"] = 1000000, -- if fruit price less u rate then we use it to auto raids
+
+    -- Devil Fruit
+
+    ["Select Devil Fruit Sniper"] = {"Ice-Ice","Sand-Sand","Dark-Dark","Quake-Quake","Light-Light"},
+    ["Auto Buy Devil Fruit Sniper"] = false,
+    ["Eat Fruit From Inventroy"] = false, -- open wait auto buy DF sinper ^
+
+    -- RAM
+
+    ["RAM Port"] = 7963,
+    ["RAM Password"] = "",
+    ["Auto Set Description"] = false,
+
+    -- CPU
+    
+    ["Select Lock Fps"] = 15,
+    ["Lock Fps Now"] = true,
+    ["White Screen"] = true
+}
