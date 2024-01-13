0474903c4845be1f79fed84b1cdb53c573617177
NightsTimeZ <84789294+NightsTimeZ@users.noreply.github.com>
2024-01-13 23:55:33 +0700

    Update Kaitun_Config.lua

--- Kaitun_Config.lua
+++ Kaitun_Config.lua
@@ -1,15 +1,65 @@
-_G.KaitunConfig = {
+{
     ["Start Farm"] = true,
-    ["PlayerHunter"] = true,
-    ["Auto Fruit"] = true,
+    -- all melee is already do with it self
+    -- auto activate list ( Auto Do List )
+    --[[ //auto activate list//
+        GodHuman, all melee
+        get god human material
+        random fruit , store fruit
+
+        / sea 1
+        auto go sea 2
+        skip farm level
+        saber
+
+        / sea 2
+        auto go sea 3
+        kill darkbeard
+        race v2
+        race v3
+        bartilo
+
+        / sea 3
+        kill soul reaper,
+        kill dough king,
+        kill cake prince
+        kill rip indra
+        kill Beautiful Pirate
+
+        dough awaking
+        elite hunter
+
+        / sea 2 - 3
+        ttk [ wado shisui sadi ]
+        legendary color haki
+        smart code redeem
+
+    ]]
+    ["Auto Fruit"] = true, -- tween to fruit
+
     ["RedeemCode Level"] = 2,
-    
-    
+
     ["Black Sreen"] = true,
+
+    ["LimitFragment"] = 100000,
+
+    -- sea 1
+    ["PlayerHunter"] = true, -- will do skip lvl too
+
     -- sea 2
     ["Auto Factory"] = true,
+    ["Rengoku"] = true,
     ["Sea 3 Hop"] = false, -- hop to find fruit
     
+    -- sea 3
+    ["CDK"] = true,
+    ["Tushita"] = true,
+    ["Yama"] = true,
+    ["Soul Guitar"] = true,
 
-    ["LimitFragment"] = 100000
+    -- Add On
+    ["Race Lock"] = "notlock", -- Human , Mink , Fishman , put other mean not lock
+
+    ["Mastery Sword"] = true, -- will farm mastery
+    ["Select Rarity"] = {"Mythical","Legendary"}, -- Common , Uncommon,Rare,Legendary,Mythical
 }
