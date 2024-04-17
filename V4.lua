d5684266e3927779aaf1aa6c79b43b242b50d652
NightsTimeZ <84789294+NightsTimeZ@users.noreply.github.com>
2024-04-18 00:41:21 +0700

    Create V4.lua

---
+++ V4.lua
@@ -0,0 +1,26 @@
+_G.V4Config = { 
+    ['Auto Race V4'] = { -- open auto evo race v4
+        ["Enable"] = true,
+        ['Helper'] = { -- Put Only 3 diff Race / ใส่เเค่ 3 เผ่า ที่ต่างกัน (Only 3 Acc!!!)
+            "",
+            "",
+            "", -- u can put 2 if u make sure main acc not have same race (สามารใส่ 2 ตัวได้ถ้าใส่ตัวฟามไม่มีตัวเผ่าเหมือนกัน)
+        },
+
+        ['Main Acc'] = { 
+            'MyId',
+            'MyId2',
+            'MyId3'
+        }
+    },
+
+    ['Auto Pull Lever'] = { -- auto pull lever / ดึงคันโยก auto
+        ['Enable'] = false,
+        ['Hop Server'] = false
+    },
+  
+    -- [[ Misc ]]
+
+    ['Close Ui'] = true,
+    ['Low Texture'] = false
+}
