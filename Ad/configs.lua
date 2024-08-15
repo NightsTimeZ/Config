cbfa2eec84613cbf1e6091b3ba7061f1871ad10a
Ayayayayayayayaya <84789294+NightsTimeZ@users.noreply.github.com>
2024-08-15 07:34:38 +0700

    dsa

---
+++ Ad/configs.lua
@@ -0,0 +1,35 @@
+getgenv().Configs = {
+	["Auto Kaitun"] = false,
+	["Kaitun Gui"] = false,
+
+	["Select Map"] = "FooshaVillage",
+	["Chapter Map"] = "Latest",
+	["Difficulty Select"] = "Normal",
+	["Friends Only"] = false,
+	["Auto Join Story"] = false,
+
+	["Select Macro"] = "Default",
+	["Macro Mode"] = "Money",
+	["Delays Next Step"] = "1", -- "0.5","0.7","1","1.5","2"
+	["Play Macro"] = false,
+	["Auto Play"] = false,
+
+	["Auto Claim Quest"] = false,
+	["Auto Skip Wave"] = true,
+	["Disable Damage"] = false,
+
+	["Auto Rematch"] = false,
+	["Auto Play Next"] = false,
+
+
+	["Rolls Banner"] = "Standard", -- Standard,Update4
+	["Rolls Mode"] = "1", -- 1,10
+	["Auto Rolls"] = false,
+
+	["Select Wave"] = 32,
+	["Instant Leave"] = false,
+
+	["Upgrade Priority"] = "Expensive", -- "Expensive", "Cheapest", "Index"
+	["Select Wave Upgrade"] = 10,
+	["Auto Upgrade"] = false,
+}
