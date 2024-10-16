614029f57f5a793a23e9739270633b14b390f6f2
Ayayayayayayayaya <84789294+NightsTimeZ@users.noreply.github.com>
2024-10-16 19:06:06 +0700

    Create ptd.lua

---
+++ ptd.lua
@@ -0,0 +1,26 @@
+getgenv().Configs = {
+
+	["Full Auto Play "] = false, -- soon
+
+	["Select Map"] = "IDK THE NAME",
+	["Select Difficult"] = "Easy", -- Easy Medium Hard Nightmare
+	["Auto Join"] = false,
+	["Automatically leave"] = true,
+
+	["Auto Spin"] = false,
+	["Auto Summon"] = false,
+
+	["Delays Next Step"] = "0.2", -- "0.1","0.2","0.3","0.4","0.5"
+	["Auto Play Macro"]  = false,
+	["Select Macro"] = "AA",
+
+	["Auto Claim Quest"] = true,
+	["Auto Claim Gift"] = true,
+	["Auto Claim Daily rewards"] = true,
+	["Auto Skip"] = true,
+	["Auto x2 Speed"] = true,
+
+	["Black screen"] = false,
+	["Fps boots"] = false,
+
+}
